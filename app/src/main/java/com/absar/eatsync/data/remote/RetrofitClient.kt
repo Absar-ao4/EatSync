3dcd36ec155d03424a15531e1093fa122c16da51
Absar-ao4 <absarali004@gmail.com>
2026-05-19 03:03:59 +0530

    Deployed the backend on railway and made it working for all devices unlike local host

--- app/src/main/java/com/absar/eatsync/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/absar/eatsync/data/remote/RetrofitClient.kt
@@ -5,8 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL="http://10.0.2.2:8082/"
-
+    private const val BASE_URL = "https://eatsync-production.up.railway.app/"
     val foodApiService:FoodApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
